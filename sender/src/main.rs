986c649468a34adeb1b1f510e90f70e9f2b1e329
Sevag Hanssian <sevag.hanssian@gmail.com>
2017-08-15 15:05:26 -0400

    Rustfmt + clippy

--- sender/src/main.rs
+++ sender/src/main.rs
@@ -3,20 +3,17 @@
 extern crate nix;
 extern crate libc;
 extern crate core;
-#[macro_use] extern crate error_chain;
+#[macro_use]
+extern crate error_chain;
 
 use nix::errno::Errno;
 use nix::sys::socket;
 use std::os::unix::io::RawFd;
 use std::net::ToSocketAddrs;
-use libc::{
-    c_void,
-    recv,
-    MSG_WAITALL,
-};
+use libc::{c_void, recv, MSG_WAITALL};
 
 static SOCK: &'static str = "127.0.0.1:8888";
-static MSGLEN: usize = 32*8;
+static MSGLEN: usize = 32 * 8;
 
 pub mod errors {
     error_chain! {
@@ -30,32 +27,42 @@ pub mod errors {
 use errors::*;
 
 quick_main!(|| -> Result<i32> {
-    let fd: Result<RawFd> = (|| {
-    let fd = socket::socket(socket::AddressFamily::Inet,
-                            socket::SockType::Stream,
-                            socket::SockFlag::empty(),
-                            socket::SOL_TCP)?;
-        let address = SOCK.to_socket_addrs()?.next()
-            .expect("Cannot convert bind to socket address");
-        socket::bind(fd, &socket::SockAddr::new_inet(socket::InetAddr::from_std(&address)))?;
+    let fd: Result<RawFd> = {
+        let fd = socket::socket(
+            socket::AddressFamily::Inet,
+            socket::SockType::Stream,
+            socket::SockFlag::empty(),
+            socket::SOL_TCP,
+        )?;
+        let address = SOCK.to_socket_addrs()?.next().expect(
+            "Cannot convert bind to socket address",
+        );
+        socket::bind(
+            fd,
+            &socket::SockAddr::new_inet(socket::InetAddr::from_std(&address)),
+        )?;
         socket::listen(fd, 16).expect("Couldn't listen for connections");
         Ok(fd)
-    })();
+    };
 
-    let fd =
-        match fd {
-            Ok(fd) => fd,
-            Err(err) => {
-                bail!("Socket error: {}", err);
-            },
-        };
+    let fd = match fd {
+        Ok(fd) => fd,
+        Err(err) => {
+            bail!("Socket error: {}", err);
+        }
+    };
 
     let mut payload = vec![0u8; 32];
 
     let mut fd_ = socket::accept(fd)?;
     loop {
         let nread = unsafe {
-            recv(fd_ , payload.as_mut_ptr() as *mut c_void, MSGLEN, MSG_WAITALL)
+            recv(
+                fd_,
+                payload.as_mut_ptr() as *mut c_void,
+                MSGLEN,
+                MSG_WAITALL,
+            )
         };
         if nread == 0 {
             fd_ = socket::accept(fd)?;
@@ -69,6 +76,11 @@ quick_main!(|| -> Result<i32> {
         let verb = String::from_utf8_lossy(&payload[8..15]);
         let body = String::from_utf8_lossy(&payload[16..]);
 
-        println!("\tversion: {:?}\n\tverb: {:?}\n\tbody: {:?}", version, verb, body);
+        println!(
+            "\tversion: {:?}\n\tverb: {:?}\n\tbody: {:?}",
+            version,
+            verb,
+            body
+        );
     }
 });
