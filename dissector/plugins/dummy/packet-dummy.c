24fe32054eab1a7e6d238e10ade720911e6ab895
Sevag Hanssian <sevag.hanssian@gmail.com>
2017-08-22 08:08:16 -0400

    Add .clang-format file + some modifications to packet-dummy.c

--- dissector/plugins/dummy/packet-dummy.c
+++ dissector/plugins/dummy/packet-dummy.c
@@ -1,12 +1,13 @@
 #include "config.h"
 
+#include <epan/dissectors/packet-tcp.h>
 #include <epan/packet.h>
-#include <wiretap/wtap.h>
 #include <epan/tvbuff-int.h>
 #include <epan/tvbuff.h>
+#include <wiretap/wtap.h>
 
-#include <stdio.h>
 #include <glib.h>
+#include <stdio.h>
 
 #include "./packet-dummy.h"
 
@@ -14,49 +15,65 @@
 
 static int proto_dummy = -1;
 static gint ett_dummy = -1;
+static int hf_dummy_version = -1;
+static int hf_dummy_body = -1;
 
-void proto_register_dummy(void);
-void proto_reg_handoff_dummy(void);
+static int
+dissect_dummy(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree, void *data);
 
-static int dissect_dummy(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree, void *data)
+void
+proto_register_dummy(void);
+void
+proto_reg_handoff_dummy(void);
+
+static int
+dissect_dummy(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree, void *data)
 {
-   ((void) data); // suppress compile warning.
+	((void)data); // suppress compile warning.
 
-   col_set_str(pinfo->cinfo, COL_PROTOCOL, "DUMMY");
-   col_clear(pinfo->cinfo,COL_INFO);
+	col_set_str(pinfo->cinfo, COL_PROTOCOL, "DUMMY");
+	col_clear(pinfo->cinfo, COL_INFO);
 
-   if (tree) { /* we are being asked for details */
-      proto_item *ti;
+	if (tree) { /* we are being asked for details */
+		proto_item *ti;
 
-      ti = proto_tree_add_item(tree, proto_dummy, tvb, 0, -1, FALSE);
-      tree = proto_item_add_subtree(ti, ett_dummy);
+		ti = proto_tree_add_item(tree, proto_dummy, tvb, 0, -1, FALSE);
+		tree = proto_item_add_subtree(ti, ett_dummy);
 
-      for (uint i = 0; i < tvb->length; ++i) {
-	    printf("%ux\n", tvb->real_data[i]);
-      }
-   }
+		for (uint i = 0; i < tvb->length; ++i) {
+			printf("%ux\n", tvb->real_data[i]);
+		}
+	}
 
-   return tvb_captured_length(tvb);
+	return tvb_captured_length(tvb);
 }
 
-void proto_register_dummy(void)
+void
+proto_register_dummy(void)
 {
-   static int *ett[] = { &ett_dummy };
+	static int *ett[] = {&ett_dummy};
 
-   proto_dummy = proto_register_protocol
-      (
-         "Dummy Protocol", /* name */
-         "DUMMY",          /* short name */
-         "dummy"           /* abbrev */
-      );
+	proto_dummy = proto_register_protocol("Dummy Protocol", /* name */
+	    "DUMMY",                                            /* short name */
+	    "dummy"                                             /* abbrev */
+	    );
 
-   proto_register_subtree_array(ett, array_length(ett));
+	static hf_register_info hf[] = {
+	    {&hf_dummy_version, {"Version", "dummy.version", FT_UINT8, BASE_HEX,
+	                            NULL, 0x0, NULL, HFILL}},
+	    {&hf_dummy_body,
+	        {"Body", "dummy.body", FT_UINT8, BASE_HEX, NULL, 0x0, NULL, HFILL}},
+	};
+
+	proto_register_field_array(proto_dummy, hf, array_length(hf));
+	proto_register_subtree_array(ett, array_length(ett));
 }
 
-void proto_reg_handoff_dummy(void)
+void
+proto_reg_handoff_dummy(void)
 {
-   static dissector_handle_t dummy_handle;
+	static dissector_handle_t dummy_handle;
 
-   dummy_handle = create_dissector_handle(dissect_dummy, proto_dummy);
-   dissector_add_uint("tcp.port", DUMMY_PORT, dummy_handle);
+	dummy_handle = create_dissector_handle(dissect_dummy, proto_dummy);
+	dissector_add_uint("tcp.port", DUMMY_PORT, dummy_handle);
 }
